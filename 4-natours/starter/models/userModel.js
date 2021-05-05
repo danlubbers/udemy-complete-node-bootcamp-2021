4ba86e8a04ec676ee703d4078565afb9a692f9a8
Dan Lubbers <lubbers.dan@gmail.com>
2021-05-05 18:48:09 -0400

    Finished Section 10: 129 - 131

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A user must have an password!'],
     minLength: [8, 'A password must have more or equal than 8 characters.'],
+    select: false, // this hides the password from db requests like Postman
   },
   passwordConfirmation: {
     type: String,
@@ -33,6 +34,7 @@ const userSchema = new mongoose.Schema({
       message: 'Passwords do not match!',
     },
   },
+  passwordChangedAt: Date,
 });
 
 userSchema.pre('save', async function (next) {
@@ -46,6 +48,31 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirmation = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword, // not hashed pw
+  userPassword // hashed pw
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    // console.log(
+    //   'Time Difference in milliseconds',
+    //   changedTimestamp,
+    //   JWTTimestamp
+    // );
+    return JWTTimestamp < changedTimestamp;
+  }
+
+  // False means not changed
+  return false;
+};
 
 const User = mongoose.model('User', userSchema);
 
