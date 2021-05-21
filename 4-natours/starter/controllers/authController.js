c12cd5d099a6e3b8b4c86b80437cdf341606a591
Dan Lubbers <lubbers.dan@gmail.com>
2021-05-21 14:03:13 -0400

    Finished Section 13: 197 - 200

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -161,16 +161,14 @@ exports.isLoggedIn = async (req, res, next) => {
 };
 
 // CLOSURE
-exports.restrictTo = (...roles) => {
-  return (req, res, next) => {
-    // roles ['admin', 'lead-guide']. role = 'user'
-    if (!roles.includes(req.user.role)) {
-      return next(
-        new AppError('You do not have permission to perform this action!', 403)
-      );
-    }
-    next();
-  };
+exports.restrictTo = (...roles) => (req, res, next) => {
+  // roles ['admin', 'lead-guide']. role = 'user'
+  if (!roles.includes(req.user.role)) {
+    return next(
+      new AppError('You do not have permission to perform this action!', 403)
+    );
+  }
+  next();
 };
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
